35cc211d16d6e06d9097557811fe5fd7d273960c
Chris <cb@strukt.com>
2016-09-06 17:55:24 +0200

    added correct words for wordRap

---
+++ Assets/scripts/LaufbandAnimation.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LaufbandAnimation : MonoBehaviour {
+
+
+	private List<Text> textElements;
+	private Text[] tmpTXTArray;
+	private int currentTXT;
+
+	void Start () {
+
+		currentTXT = 0;
+
+		textElements = new List<Text>();
+
+		//get all children TXT elements
+		tmpTXTArray = gameObject.GetComponentsInChildren<Text> ();
+		for (int i = 1; i < tmpTXTArray.Length; i++) {
+			textElements.Add(tmpTXTArray[i]);
+		}
+			
+		//fade all out
+		for (int i = 0; i < textElements.Count; i++) {
+			textElements [i].CrossFadeAlpha (0F, 0F, false);
+		}
+	}
+
+	void Update () {
+	
+	}
+
+	public void ShowTXT(int i) {
+		if (i != currentTXT) {
+			textElements [i].CrossFadeAlpha (1F, 0.5F, false);
+			textElements [currentTXT].CrossFadeAlpha (0F, 0.5F, false);
+			currentTXT = i;
+		}
+	}
+
+
+
+
+
+
+}
