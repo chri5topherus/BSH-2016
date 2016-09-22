388fd1448c629ff7896ca7411d9eb50ca7748ef3
Chris <cb@strukt.com>
2016-09-22 09:54:28 +0200

    added hinterberger wordrap

---
+++ Assets/HinterbergerWordRap.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+
+public class HinterbergerWordRap : MonoBehaviour {
+
+	private List<Text> textElements;
+	private Text[] tmpTXTArray;
+	private int currentTXT;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
